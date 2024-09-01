d38a3c5ae327c561a12fc8ec77851b5088046f3c
Mir449 <77783787+Mir449@users.noreply.github.com>
2024-09-01 15:34:05 +0500

    Add files via upload

---
+++ Automation-Framework-with-BDD-and-Data-Drvien-Aproach-With-Test-Driven-Aproach-Public/Automation-Framework-with-BDD-and-Data-Drvien-Aproach-With-Test-Driven-Aproach-master/Common/Context.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renci.SshNet;
+using Renci.SshNet.Common;
+
+namespace Dependency_Verification.Common
+{
+    internal class Context
+    {
+        static HttpClient client;
+        static SshClient sshClient;
+        public Context() { 
+        client = new HttpClient();
+        }
+
+
+        public void connectssh() {
+
+            sshClient = new SshClient(System.Configuration.ConfigurationManager.AppSettings["CoreIP"], System.Configuration.ConfigurationManager.AppSettings["CoreUser"], System.Configuration.ConfigurationManager.AppSettings["CorePwd"]);
+            sshClient.Connect();
+            var command = sshClient.CreateCommand("bash --login -c 'cd Core;p2r p2_a_p2p 23153631236'");
+            //     var command2 = sshClient.RunCommand("cd Core;p2r p2_a_p2p 23153631236");
+
+            //  var rslt = command.BeginExecute();
+
+           var rslt= command.Execute();
+         /* try
+            {
+                IAsyncResult asyncResult = command.BeginExecute(null, null);
+                bool completed = asyncResult.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(10)); // Set a timeout of 10 seconds
+                if (completed)
+                {
+                    string result = command.EndExecute(asyncResult);
+                    // Process the result
+                }
+
+            }
+            catch(Exception ex)
+            {
+
+            }*/
+            /*using (var reader = new StreamReader(command.OutputStream))
+            {
+
+                // Read the command output in a loop
+                while (!result.IsCompleted || !reader.EndOfStream)
+                {
+                    // Read a line from the output
+                    System.Diagnostics.Debug.WriteLine("valure");
+                        string line = reader.ReadLine();
+                    // Process the line as desired (e.g., print it)
+                    System.Diagnostics.Debug.WriteLine(line);
+                }
+            }
+
+            command.EndExecute(result);
+*/
+            sshClient.Disconnect();
+
+        }
+
+        public void baseurl(string url) { 
+        client.BaseAddress = new Uri(url);
+        }
+
+        public void requestheaders()
+        {
+
+        }
+    }
+}
---
+++ Automation-Framework-with-BDD-and-Data-Drvien-Aproach-With-Test-Driven-Aproach-Public/Automation-Framework-with-BDD-and-Data-Drvien-Aproach-With-Test-Driven-Aproach-master/Driver/Driver.cs
@@ -0,0 +1,145 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Verification.Driver
+{
+    public class Drivers
+    {
+        static IWebDriver driver;
+        static WebDriverWait waitdriver;
+        Dependency_Verification.Factory.Factory obj = new Factory.Factory();
+
+        public Drivers()
+        {
+        }
+
+        public Drivers(bool flag)
+        {
+            if (flag) {
+                if (System.Configuration.ConfigurationManager.AppSettings["Browser"].ToString() =="chrome")
+                {
+                    String driverexepath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\112.0.5615.138\\chromedriver.exe";
+                driver = new ChromeDriver(driverexepath);
+                }
+                else
+                {
+                    driver = new FirefoxDriver();
+                }
+                waitdriver = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            }
+        }
+
+        public WebDriverWait wait()
+        {
+            return waitdriver;
+        }
+        public IWebDriver Instance()
+        {
+            return driver;
+        }
+
+        public void GotoUrl(String url)
+        {
+            switch (System.Configuration.ConfigurationManager.AppSettings["Browser"])
+            {
+                case "firefox":
+                    {
+                        try
+                        {
+                            driver.Navigate().GoToUrl(url);
+                        }
+                        catch (Exception)
+                        {
+                            btn(obj.fetch_Element("Login_Advance_chrome").Locator);
+                            btn(obj.fetch_Element("Login_Accept_chrome").Locator);
+                        }
+                    }
+                    break;
+                case "chrome":
+                    {
+                        try
+                        { 
+                        driver.Navigate().GoToUrl(url);
+                        }
+                        catch(Exception)
+                        {
+                            btn(obj.fetch_Element("Login_Advance_chrome").Locator);
+                            btn(obj.fetch_Element("Login_Accept_chrome").Locator);
+                        }
+                        break;
+                    }
+                default:
+                    throw new Exception("Wrong Type in App config on Browser, should be either firefox or chrome");
+            }
+
+
+
+
+            //Assert.True(driver.FindElement(By.TagName("body")).Text.Contains(u));
+            waitdriver.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.TagName("body")));
+            
+        }
+
+
+        public void txtbox(String Value, String Xpath)
+        {
+            waitdriver.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(Xpath))).SendKeys(Value);
+        }
+
+
+        public void pageisready()
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Instance();
+            if (js.ExecuteScript("return document.readyState").ToString().Equals("complete"))
+            {
+                return;
+            }
+        }
+        public int iterator =0;
+        public void btn(String Xpath)
+        {
+            try
+            {
+                 iterator++;
+                //Thread.Sleep(20000);
+                pageisready();
+                // System.Diagnostics.Debug.WriteLine("This is Xpath : "+Xpath);
+
+                waitdriver.Until(ExpectedConditions.ElementToBeClickable(By.XPath(Xpath)));
+                {
+                    waitdriver.Until(ExpectedConditions.ElementIsVisible(By.XPath(Xpath)));
+                    {
+                        waitdriver.Until(ExpectedConditions.ElementToBeClickable(By.XPath(Xpath)));
+                        {
+                            IWebElement link = waitdriver.Until(ExpectedConditions.ElementToBeClickable(By.XPath(Xpath)));
+                            link.Click();
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+             Thread.Sleep(100);
+            btn(Xpath);
+
+                if (iterator == 10)
+                {
+                    return;
+                }
+            }
+        }
+
+
+
+    }
+}
